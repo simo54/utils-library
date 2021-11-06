ce996144f1222ab2b290827147ad0ac149c11f05
scava <sorzesimone1@gmail.com>
2021-11-06 20:27:49 +0100

    created getLastName

--- src/strings/getFirstName.ts
+++ src/strings/getFirstName.ts
@@ -1,3 +1,7 @@
 export function getFirstName(name: string) {
   return name.substr(0, name.indexOf(" "));
 }
+
+export function getLastName(name: string) {
+  return name.substr(name.indexOf(" "));
+}
